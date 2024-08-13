5e9b22b2039fdd485c58090e8e8bcdf77be71652
A-Aref <abdallahahmadaref@gmail.com>
2024-08-13 19:11:13 +0300

    fix port

--- client/Inventory_CURT/vite.config.js
+++ client/Inventory_CURT/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy:{
-      "/api": "https://inventorycurtbackend-abd-allah-ahmad-arefs-projects.vercel.app/",
+      "/api": "http://localhost:5000",
     },
   },
   plugins: [react()],
